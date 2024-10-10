56cf9cfe9548eebe03b2a8d6fd53fc63d88a16fd
deoresulakshana <deoresulakshana@gmail.com>
2024-10-10 14:57:01 +0530

    Add project files.

---
+++ EntityLib/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLib
+{
+    public class Employee
+    {
+        internal static int id=0;
+      internal static void GenerateEmployeeID()
+        { 
+        id= id + 1; 
+           
+        }
+        public int Empid { get; internal set; }
+        public string Empname { get; set; }
+        public string City { get; set; }
+
+
+    }
+
+
+
+   public class EmployeeOperations
+    {
+
+        static List<Employee> emplist = new List<Employee>();    
+        public int AddNewEmployee(Employee emp)
+        {
+            Employee.GenerateEmployeeID();
+            emp.Empid=Employee.id;
+            emplist.Add(emp);
+            return emp.Empid;
+        }
+        public List<Employee> AllEmplist()
+        { 
+        
+        return emplist;
+        
+        
+        }
+    
+    }
+}
---
+++ EntityLib/Products.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EntityLib
+{
+    public class Products
+    {
+        public int Prodid { get; set; }
+        public string Prodname { get; set; }
+        public float Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public double Qty { get; set; }
+
+
+    }
+
+
+    public class ProductOperations {
+
+
+        static List<Products> prodList=new List<Products>();
+        public void SetDataForProduct(Tuple<int, string, float, bool, double> pdata)
+        {
+
+            prodList.Add(new Products { Prodid=pdata.Item1, Prodname=pdata.Item2, Price=pdata.Item3,IsAvailable=pdata.Item4,Qty=pdata.Item5});  
+        }
+
+        public List<Products> ShowAllProductData() 
+        {
+            return prodList;
+
+
+        }
+
+
+        public Tuple<int, string, float, bool, double> FindProduct(int prodid)
+        {
+            Products p = prodList.Find(p1 => p1.Prodid == prodid);
+           Tuple <int,string,float,bool,double> foundProduct= Tuple.Create<int, string, float, bool, double>(p.Prodid,p.Prodname,p.Price,p.IsAvailable,p.Qty);
+            return foundProduct;
+
+        }
+
+
+
+
+    }
+}
